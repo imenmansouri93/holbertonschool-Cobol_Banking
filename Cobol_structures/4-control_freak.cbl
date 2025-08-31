44a7b077fafa16b6cb0b4df92039943978f8d310
imenmansouri93 <imennmansourii@gmail.com>
2025-08-31 15:01:00 +0100

    new

--- Cobol_structures/4-control_freak.cbl
+++ Cobol_structures/4-control_freak.cbl
@@ -3,10 +3,10 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 BALANCE           PIC S9(6)V99 VALUE 1000.00.
-       01 DISPLAY-BALANCE   PIC Z,ZZZ.99.
-       01 OPERATION         PIC X VALUE SPACE.
-       01 AMOUNT            PIC S9(6)V99 VALUE 0.
+       01  BALANCE           PIC S9(6)V99 VALUE 1000.00.
+       01  DISPLAY-BALANCE   PIC Z,ZZZ.99.
+       01  OPERATION         PIC X VALUE SPACE.
+       01  AMOUNT            PIC S9(6)V99 VALUE 0.
 
        PROCEDURE DIVISION.
        MAIN-PARA.
@@ -21,6 +21,7 @@
                        ACCEPT AMOUNT
                        ADD AMOUNT TO BALANCE
                        DISPLAY "Deposit successful."
+                       DISPLAY SPACE
                    WHEN "W"
                        DISPLAY "Enter withdrawal amount: "
                        ACCEPT AMOUNT
@@ -30,16 +31,17 @@
                        ELSE
                            DISPLAY "Insufficient funds."
                        END-IF
+                       DISPLAY SPACE
                    WHEN "B"
                        MOVE BALANCE TO DISPLAY-BALANCE
                        DISPLAY "Current Balance: " DISPLAY-BALANCE
+                       DISPLAY SPACE
                    WHEN "Q"
                        DISPLAY "Goodbye!"
                    WHEN OTHER
                        DISPLAY "Invalid option. Please try again."
+                       DISPLAY SPACE
                END-EVALUATE
-
-               DISPLAY SPACE
            END-PERFORM
 
            STOP RUN.
