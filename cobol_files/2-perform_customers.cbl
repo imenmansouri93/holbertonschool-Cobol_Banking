cc161ea1ca3a4ccac7198c0aa75860958a65e115
imenmansouri93 <imennmansourii@gmail.com>
2025-10-13 15:37:25 +0100

    new

--- cobol_files/2-perform_customers.cbl
+++ cobol_files/2-perform_customers.cbl
@@ -13,11 +13,10 @@
        01 CUSTOMER-RECORD       PIC X(33).
 
        WORKING-STORAGE SECTION.
-       01 WS-CUST-ID            PIC 9(5).
+       01 WS-CUST-ID-INPUT      PIC X(5).
        01 WS-CUST-FNAME         PIC X(10).
        01 WS-CUST-LNAME         PIC X(10).
-       01 WS-BALANCE            PIC 9(5)V99.
-       01 WS-BALANCE-STR        PIC X(8).
+       01 WS-BALANCE-INPUT      PIC X(8).
        01 WS-ANSWER             PIC X VALUE "Y".
 
        PROCEDURE DIVISION.
@@ -25,22 +24,19 @@
            OPEN EXTEND CUSTOMER-FILE
            PERFORM UNTIL WS-ANSWER NOT = "Y"
                DISPLAY "Enter Customer ID (5 digits): "
-               ACCEPT WS-CUST-ID
+               ACCEPT WS-CUST-ID-INPUT
                DISPLAY "Enter First Name (max 10 characters): "
                ACCEPT WS-CUST-FNAME
                DISPLAY "Enter Last Name (max 10 characters): "
                ACCEPT WS-CUST-LNAME
                DISPLAY "Enter Balance (less than 99999.99): "
-               ACCEPT WS-BALANCE
+               ACCEPT WS-BALANCE-INPUT
 
-               MOVE WS-BALANCE TO WS-BALANCE-STR
-
-               STRING
-                   WS-CUST-ID DELIMITED BY SIZE
-                   WS-CUST-FNAME DELIMITED BY SIZE
-                   WS-CUST-LNAME DELIMITED BY SIZE
-                   WS-BALANCE-STR DELIMITED BY SIZE
-               INTO CUSTOMER-RECORD
+               MOVE SPACES TO CUSTOMER-RECORD
+               MOVE WS-CUST-ID-INPUT TO CUSTOMER-RECORD(1:5)
+               MOVE WS-CUST-FNAME TO CUSTOMER-RECORD(6:10)
+               MOVE WS-CUST-LNAME TO CUSTOMER-RECORD(16:10)
+               MOVE WS-BALANCE-INPUT TO CUSTOMER-RECORD(26:8)
 
                WRITE CUSTOMER-RECORD
 
