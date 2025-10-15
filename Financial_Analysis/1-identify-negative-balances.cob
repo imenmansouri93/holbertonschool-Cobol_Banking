59f3b0af816efd26920e4b67e67d6097ec1be622
imenmansouri93 <imennmansourii@gmail.com>
2025-10-15 16:42:11 +0100

    new

--- Financial_Analysis/1-identify-negative-balances.cob
+++ Financial_Analysis/1-identify-negative-balances.cob
@@ -1,37 +1,36 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. IDENTIFYNEGATIVE.
-
+       PROGRAM-ID. IDENTIFY-NEGATIVE-BALANCES.
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT BALANCE-FILE ASSIGN TO 'balances.idx'
+           SELECT BALANCE-FILE ASSIGN TO "balances.idx"
                ORGANIZATION IS LINE SEQUENTIAL.
-
        DATA DIVISION.
        FILE SECTION.
-       FD  BALANCE-FILE.
-       01  BALANCE-RECORD.
-           05 CUSTOMER-ID        PIC X(7).
-           05 FILLER             PIC X(3).
-           05 BALANCE-AMOUNT     PIC S9(7)V99.
-
+       FD BALANCE-FILE.
+       01 BALANCE-RECORD.
+           05 ACCOUNT-ID         PIC X(9).
+           05 BALANCE-STRING     PIC X(10).
        WORKING-STORAGE SECTION.
-       01 EOF-FLAG               PIC X VALUE 'N'.
-
+       01 EOF-FLAG              PIC X VALUE "N".
+           88 END-OF-FILE       VALUE "Y".
+           88 NOT-EOF           VALUE "N".
+       01 BALANCE-NUMERIC       PIC S9(5)V99 COMP-3.
        PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
            OPEN INPUT BALANCE-FILE
-
-           PERFORM UNTIL EOF-FLAG = 'Y'
+           PERFORM UNTIL END-OF-FILE
                READ BALANCE-FILE
                    AT END
-                       MOVE 'Y' TO EOF-FLAG
+                       SET END-OF-FILE TO TRUE
                    NOT AT END
-                       IF BALANCE-AMOUNT < 0
-                           DISPLAY "Account " CUSTOMER-ID
-                                   "   - Balance: " BALANCE-AMOUNT
+                       MOVE FUNCTION NUMVAL(BALANCE-STRING) TO 
+                       BALANCE-NUMERIC
+                       IF BALANCE-NUMERIC < 0
+                           DISPLAY "Account " ACCOUNT-ID " - Balance: " 
+                           BALANCE-STRING
                        END-IF
                END-READ
            END-PERFORM
-
            CLOSE BALANCE-FILE
            STOP RUN.
