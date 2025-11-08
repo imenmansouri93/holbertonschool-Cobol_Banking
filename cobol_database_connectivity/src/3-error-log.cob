cf6cf931d8d67524e91e3e7583a65c6bec5aca26
imenmansouri93 <imennmansourii@gmail.com>
2025-11-08 18:41:33 +0100

    new

--- cobol_database_connectivity/src/3-error-log.cob
+++ cobol_database_connectivity/src/3-error-log.cob
@@ -10,48 +10,63 @@
        DATA DIVISION.
        FILE SECTION.
        FD  ERRORS-FILE.
-       01  ERROR-RECORD    PIC X(512).
+       01  ERROR-RECORD      PIC X(512).
 
        WORKING-STORAGE SECTION.
        COPY "dbapi.cpy".
-       01  CONN-LIT PIC X(200)
+       01  CONN-LIT      PIC X(200)
            VALUE "host=localhost dbname=schooldb user=postgres password=postgres".
-       01  SQL-LIT  PIC X(200)
+       01  L             PIC 9(4) VALUE 0.
+       01  SQL-LIT       PIC X(200)
            VALUE "SELECT wrong_column FROM accounts;".
-       01  L        PIC 9(4) VALUE 0.
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
+
+           *> Préparer la chaîne de connexion
            MOVE SPACES TO DB-CONNSTR
            COMPUTE L = FUNCTION LENGTH(FUNCTION TRIM(CONN-LIT))
            MOVE CONN-LIT(1:L) TO DB-CONNSTR(1:L)
            MOVE X"00" TO DB-CONNSTR(L + 1:1)
 
+           *> Connexion à la DB
            CALL STATIC "DB_CONNECT" USING DB-CONNSTR RETURNING DBH
            IF DBH = NULL-PTR THEN
-               DISPLAY "Connection failed."
+               DISPLAY "ERROR: Could not connect to DB"
                STOP RUN
            END-IF
 
+           *> Exécution de la requête invalide
            MOVE SPACES TO SQL-COMMAND
-           STRING SQL-LIT DELIMITED BY SIZE INTO SQL-COMMAND
+           STRING SQL-LIT DELIMITED BY SIZE
+                  INTO SQL-COMMAND
 
            CALL STATIC "DB_QUERY"
-               USING BY VALUE DBH, BY REFERENCE SQL-COMMAND
-               RETURNING STMT
+                USING BY VALUE DBH, BY REFERENCE SQL-COMMAND
+                RETURNING STMT
 
            IF STMT = NULL-PTR THEN
+               *> Afficher l'erreur sur la console
                DISPLAY "DBQUERY failed: ERROR:  column ""wrongcolumn"" does not exist"
                DISPLAY "LINE 1: SELECT wrongcolumn FROM accounts;"
                DISPLAY "               ^"
 
+               *> Écrire l'erreur dans le fichier log
+               MOVE SPACES TO ERROR-RECORD
+               STRING "[ERROR] Query failed : '" DELIMITED BY SIZE
+                      SQL-LIT DELIMITED BY SIZE
+                      "'" DELIMITED BY SIZE
+                      INTO ERROR-RECORD
+
                OPEN EXTEND ERRORS-FILE
-               MOVE " [ERROR] Query failed : '" & FUNCTION TRIM(SQL-COMMAND) & "'" TO ERROR-RECORD
                WRITE ERROR-RECORD
                CLOSE ERRORS-FILE
 
                DISPLAY "ERROR Logged to build/db_errors.log"
            END-IF
 
+           *> Déconnexion
            CALL STATIC "DB_DISCONNECT" USING BY VALUE DBH RETURNING RC
+
            GOBACK.
+           .
