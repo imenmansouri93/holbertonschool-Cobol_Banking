46106c3ff013e533e37634617639b24327751770
imenmansouri93 <imennmansourii@gmail.com>
2025-11-08 18:20:08 +0100

    new

--- cobol_database_connectivity/src/2-error-demo.cob
+++ cobol_database_connectivity/src/2-error-demo.cob
@@ -13,20 +13,17 @@
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
 
-           *> --- Préparer la chaîne de connexion ---
            MOVE SPACES TO DB-CONNSTR.
            COMPUTE L = FUNCTION LENGTH(FUNCTION TRIM(CONN-LIT)).
            MOVE CONN-LIT(1:L) TO DB-CONNSTR(1:L).
            MOVE X"00" TO DB-CONNSTR(L + 1:1).
 
-           *> --- Connexion à la DB ---
            CALL STATIC "DB_CONNECT" USING DB-CONNSTR RETURNING DBH.
            IF DBH = NULL-PTR THEN
                DISPLAY "ERROR: Connection failed."
                STOP RUN
            END-IF.
 
-           *> --- Exécution de la requête invalide ---
            MOVE SPACES TO SQL-COMMAND.
            STRING "SELECT nope FROM accounts"
                DELIMITED BY SIZE
@@ -36,12 +33,10 @@
                USING BY VALUE DBH, BY REFERENCE SQL-COMMAND
                RETURNING STMT.
 
-           *> --- Vérification des erreurs ---
            IF STMT = NULL-PTR THEN
-               DISPLAY "DBQUERY failed: ERROR: column ""nope"" does not exist"
+               DISPLAY "ERROR: Query failed : '" FUNCTION TRIM(SQL-COMMAND) ";'"
            END-IF.
 
-           *> --- Déconnexion ---
            CALL STATIC "DB_DISCONNECT" USING BY VALUE DBH RETURNING RC.
 
            GOBACK.
